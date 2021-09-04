45b676d1232dc9293648785d1ace69d5e887a929
shin_hiroshi <hiroshi.shin@appdevteam.work>
2021-09-04 11:03:21 +0900

    applied props

--- src/sections/intro/Profile.js
+++ src/sections/intro/Profile.js
@@ -24,85 +24,108 @@ export default function Profile(lng) {
     .finally(() => setLoading(false))
   }, [])
 
-  let mainContent
-  if (!isLoading && !data['is_error']) {
-    const address = data.address
+  // Init the layout
+  let ItemList
+  const MainContent = () => (
+    <Col>
+      <Card.Body>
+        <Container className="intro-container">
+          <Row className="title-row">
+            <Col><Card.Title>{ data['title'] }</Card.Title></Col>
+          </Row>
+          { data['is_error'] 
+          ? <p>Failed to load</p> 
+          : isLoading 
+          ? <ActivityIndicator /> 
+          : <ItemList /> }
+        </Container>
+      </Card.Body>
+    </Col>
+  )
 
-    let strAddress, transit, stationAccess, strBirth, validity
-    const addressParts = [address.town, address.ward, address.city, address.prefecture, address.country]
-    transit = `${data['transit'].line} ${data['transit'].station}`
-    const birth = new Date(data['birth'])
-    const identity = data['identity']
-
-    const txtIntro = []
-    data['summary'].split('\\n\\n').forEach((e, i) => {
-      txtIntro.push(<p key={i}>{e}</p>)
-    })
-
-    let colItemWidth
-
-    if (lang == 'jp') {
-      addressParts.pop()
-      strAddress = addressParts.reverse().join(' ')
-      stationAccess = data['transit'].access.map(x => `${x.method}${x.time}分`).join(', ')
-      strBirth = LocalTime(birth)
-      validity = LocalTime(new Date(identity['expire_on']))
-
-      colItemWidth = 2
-    } else {
-      strAddress = addressParts.join(', ')
-      transit += ' Line'
-      stationAccess = data['transit'].access.map(x => `${x.time} mins ${x.method}`).join(', ')
-      strBirth = LocalTime(birth, 'en-US', 'Asia/Taipei')
-      validity = LocalTime(new Date(identity['expire_on']), 'en-US', 'Japan')
-
-      colItemWidth = 3
-    }
-
-    mainContent = (
-      <Container className="intro-container">
-        <Row className="title-row">
-          <Col><Card.Title>{ data['title'] }</Card.Title></Col>
-        </Row>
-        <Row>
-          <Translation>{ t => <Col xs={ colItemWidth }><b>{t('address')}</b></Col>}</Translation>
-          <Col>〒{address['postal_code']}　{strAddress}</Col>
-        </Row>
-        <Row>
-          <Translation>{ t => <Col xs={ colItemWidth }><b>{t('transit')}</b></Col>}</Translation>
-          <Col>{transit}　{stationAccess}</Col>
-        </Row>
-        <Row>
-          <Translation>{ t => <Col xs={ colItemWidth }><b>{t('birth')}</b></Col>}</Translation>
-          <Col>{strBirth}　{Wareki(birth, lang)}</Col>
-        </Row>
-        <Row>
-          <Translation>{ t => <Col xs={ colItemWidth }><b>{t('identity')}</b></Col>}</Translation>
-          <Translation>
-            { t => 
-              <Col>
-                {t('nationality')}：{identity['nationality']}<br/>
-                {t('visa')}：{identity['visa']}<br/>
-                {t('validity')}：{validity}
-              </Col>
-            }
-          </Translation>
-          
-        </Row>
-        <Row>
-          <Translation>{ t => <Col xs={ colItemWidth }><b>{t('experience')}</b></Col>}</Translation>
-          <Col>{Experience(data['experience'], lang)}</Col>
-        </Row>
-        <Row>
-          <Translation>{ t => <Col xs={ colItemWidth }><b>{t('intro')}</b></Col>}</Translation>
-          <Col>{ txtIntro }</Col>
-        </Row>
-      </Container>
-    )
+  // Loading or error: return the layout
+  if (isLoading || data['is_error']) {
+    return (<MainContent />)
   }
-  
 
-  return(<Col>{ isLoading 
-    ? <ActivityIndicator/> 
-    : <Card.Body>{mainContent}</Card.Body> }</Col>)
+  // Finished loading: display the data with the layout
+  // Address
+  const address = data.address
+  let strAddress, transit, stationAccess, strBirth, validity
+  const addressParts = [address.town, address.ward, address.city, address.prefecture, address.country]
+  transit = `${data['transit'].line} ${data['transit'].station}`
+  // Birthday, Identity
+  const birth = new Date(data['birth'])
+  const identity = data['identity']
+  // Intro: as paragraphs
+  const txtIntro = []
+  data['summary'].split('\\n\\n').forEach((e, i) => {
+    txtIntro.push(<p key={i}>{e}</p>)
+  })
+
+  // Create the strings and determine the width
+  let colItemWidth
+  if (lang == 'jp') {
+    addressParts.pop()
+    strAddress = addressParts.reverse().join(' ')
+    stationAccess = data['transit'].access.map(x => `${x.method}${x.time}分`).join(', ')
+    strBirth = LocalTime(birth)
+    validity = LocalTime(new Date(identity['expire_on']))
+    colItemWidth = 2
+  } else {
+    strAddress = addressParts.join(', ')
+    transit += ' Line'
+    stationAccess = data['transit'].access.map(x => `${x.time} mins ${x.method}`).join(', ')
+    strBirth = LocalTime(birth, 'en-US', 'Asia/Taipei')
+    validity = LocalTime(new Date(identity['expire_on']), 'en-US', 'Japan')
+    colItemWidth = 3
+  }
+
+  // Inside layouts
+  // Row
+  const ItemRow = (props) => {
+    const { name, val } = props.item
+    return (<Row>
+      <Translation>
+        { t => 
+          <Col xs= { props.width }>
+            <b>{ t(name) }</b>
+          </Col>
+        }
+      </Translation>
+      <Col>{ val }</Col>
+    </Row>)
+  }
+
+  // Identity row details
+  const idStatus = ( 
+    <Translation>
+      { t => 
+        <div>
+          {t('nationality')}：{identity['nationality']}<br/>
+          {t('visa')}：{identity['visa']}<br/>
+          {t('validity')}：{validity}
+        </div>
+      }
+    </Translation>
+  )
+
+  // Row details
+  const items = [
+    { name: "address", val: `〒${address['postal_code']} ${strAddress}` }, 
+    { name: "transit", val: `${transit} ${stationAccess}`}, 
+    { name: "birth", val: `${strBirth} ${Wareki(birth, lang)}` }, 
+    { name: "identity", val: idStatus }, 
+    { name: "experience", val: Experience(data['experience'], lang) }, 
+    { name: "intro", val: txtIntro}
+  ]
+  
+  // Rows
+  ItemList = () => (
+    items.map((item, i) => (<ItemRow key={ `row-${i}` } 
+      width={ colItemWidth }
+      item={ item }/>))
+  )
+
+  return (<MainContent />)
 }
